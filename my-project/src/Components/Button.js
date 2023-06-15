b5faf3ca379dc2ba3207759c0ec82b61ea40cb2f
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-15 15:11:06 -0700

    created button file

---
+++ my-project/src/Components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div>
+       <button className="linkedin">LinkedIN</button>
+       <button className="email">Email</button>
+    </div>
+  )
+}
+
+export default Button
+
