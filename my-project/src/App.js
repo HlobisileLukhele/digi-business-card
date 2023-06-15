bb24f40f2643cd240b845be2228a3a9b6f9fbfed
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-15 05:34:02 -0700

    independencies and files created

---
+++ my-project/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./Components/Header"
+import Headshot from "../images/head-shot"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Header />
+       </Header>
+     
+    </div>
+  );
+}
+
+export default App;
