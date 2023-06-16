7165d62f83f202265aaf5bd189422496d624d1ca
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-16 16:07:02 -0700

    increased font size

--- my-project/src/Components/Header.js
+++ my-project/src/Components/Header.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
   render() {
     return (
       <div>
-        <h2 className="name font-bold text-00 font-family:sans-serif pt-4 ">Hlobisile  Lukhele</h2>
+        <h2 className="name font-bold text-400 font-family:sans-serif pt-4 ">Hlobisile  Lukhele</h2>
         <h4 className="dev-name bold text-orange-600 font-family:sans-serif pt-2 px-4 py-4 ">Fronted Developer</h4>
         
       </div>
