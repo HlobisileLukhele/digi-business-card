a433d45b87ed2d87df838fa699c5a7f350db50a7
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-16 16:07:48 -0700

    fixed spelling

--- my-project/src/Components/About.js
+++ my-project/src/Components/About.js
@@ -3,10 +3,10 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h3>About </h3>
-      <p >I am web developer with a passion for solving problems using 
-        Techh skill and recourses, helping small business leverage their impact through
-        the use pf internet presence.
+      <h3 className="about font-bold  text-400 font-family:sans-serif pt-4 px-2 py-2 ">About </h3>
+      <p className="info" >I am web developer with a passion for solving problems using 
+        Tech skill and recourses, helping small business leverage their impact through
+        the use of internet presence.
       </p>
     </div>
   )
