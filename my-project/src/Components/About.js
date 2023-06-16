41d0af326dab8e01878357d9b2c426e92a357271
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-16 14:46:26 -0700

    Created about file and added about info

---
+++ my-project/src/Components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h3>About </h3>
+      <p >I am web developer with a passion for solving problems using 
+        Techh skill and recourses, helping small business leverage their impact through
+        the use pf internet presence.
+      </p>
+    </div>
+  )
+}
+
+export default About
