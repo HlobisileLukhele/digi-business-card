5588904d0bd8cdc66d69fd526c1dccad78eea100
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-16 14:42:39 -0700

    added two buttons and styled them

--- my-project/src/Components/Button.js
+++ my-project/src/Components/Button.js
@@ -3,8 +3,14 @@ import React from 'react'
 function Button() {
   return (
     <div>
-       <button className="linkedin">LinkedIN</button>
-       <button className="email">Email</button>
+        
+       <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
+        LinkedIN
+       </button>
+       <button className="bg-white-400 hover:bg-black-600 text-black font-bold py-2 px-2 rounded ">
+         Email
+       </button>
+       
     </div>
   )
 }
