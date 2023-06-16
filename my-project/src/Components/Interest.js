9e5fcd8ad1aad8ab76a22e4ea4571f35ef8ea39e
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-16 16:06:18 -0700

    created interest file and added interest info

---
+++ my-project/src/Components/Interest.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Interest() {
+  return (
+    <div>
+      <h3>Interest</h3>
+      <p>
+        As a person who enjoys created outlet from music,art to Technology it provides
+        me the opportunity to put my programmng skills to work while exploring and 
+        learning more about the field on it own.
+      </p>
+    </div>
+  )
+}
+
+export default Interest
