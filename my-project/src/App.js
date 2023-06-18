a8b5136d77c828d4753f7af8853255e74f23030e
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-18 14:20:57 -0700

    imported icon file

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -4,14 +4,19 @@ import Button from "./Components/Button"
 import About from "./Components/About"
 import Interest from "./Components/Interest"
 import Hlobisile from "./Hlobisile.png"
+import Icons from "./Components/Icons"
+
+
 function App() {
   return (
     <div className="App bg-black">
+
       <img src={Hlobisile} className="Head-shot" alt="me-smiling " />
         <Header />
        <Button />
        <About />
        <Interest />
+       <Icons />
 
     </div>
   );
