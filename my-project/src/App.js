e2994a7d94a3612b1d50c3caef82920c68446b16
HlobisileLukhele <n.thwala03@gmail.com>
2023-06-18 12:23:59 -0700

    imported the image and rendered it

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 import Button from "./Components/Button"
 import About from "./Components/About"
 import Interest from "./Components/Interest"
-
-
+import Hlobisile from "./Hlobisile.png"
 function App() {
   return (
     <div className="App bg-black">
-    <img src="../images/head-shot.png" alt="myself smiling" />
-      <Header />
+      <img src={Hlobisile} className="Head-shot" alt="me-smiling " />
+        <Header />
        <Button />
        <About />
        <Interest />
 
-        
     </div>
   );
 }
